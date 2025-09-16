b856d664c48214fdaef81c506e5a01c10c7be278
Aditya Eka Pranata <aepranata@gmail.com>
2025-09-16 13:53:40 +0700

    LineageParts: Dont't remove Network Traffic preference
    Since we mo Network Traffic position not in center anymore, so preference must not removed.
    
    This partially revert commit 9fdb4c758d545ca12bb9a92e6063d01b180cf5d5
    
    Change-Id: I22dabd95c099f0981e5a821088f7d754f3a89de3
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- src/org/lineageos/lineageparts/statusbar/StatusBarSettings.java
+++ src/org/lineageos/lineageparts/statusbar/StatusBarSettings.java
@@ -80,9 +80,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         mNetworkTrafficPref = findPreference(NETWORK_TRAFFIC_SETTINGS);
 
         mHasCenteredCutout = DeviceUtils.hasCenteredCutout(getActivity());
-        if (mHasCenteredCutout) {
-            getPreferenceScreen().removePreference(mNetworkTrafficPref);
-        }
 
         mStatusBarAmPm = findPreference(STATUS_BAR_AM_PM);
         mStatusBarClock = findPreference(STATUS_BAR_CLOCK_STYLE);
